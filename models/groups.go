ea18db2019aced2c3085c78a6c5a39ad92c16f5a
alex breadman <3dukltd@gmail.com>
2023-07-07 21:16:40 +0100

    groups

--- models/groups.go
+++ models/groups.go
@@ -3,18 +3,20 @@ package models
 import "github.com/google/uuid"
 
 type Group struct {
-	ID      string
-	Admin   string
-	Name    string
-	Region  string
-	Members []string
+	ID          string   `json:"id"`
+	Admin       string   `json:"admin"`
+	Name        string   `json:"name"`
+	Description string   `json:"description"`
+	Region      string   `json:"region"`
+	Members     []string `json:"members"`
 }
 
-func NewGroup(admin, name, region string) *Group {
+func NewGroup(admin, region, name, descr string) *Group {
 	return &Group{
-		ID:     uuid.NewString(),
-		Admin:  admin,
-		Name:   name,
-		Region: region,
+		ID:          uuid.NewString(),
+		Admin:       admin,
+		Name:        name,
+		Description: descr,
+		Region:      region,
 	}
 }
