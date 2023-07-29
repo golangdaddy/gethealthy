7d1743917c39ee670846857202ccefa1d69d8a08
alex breadman <3dukltd@gmail.com>
2023-07-29 19:26:13 +0100

    add mail

--- models/mail.go
+++ models/mail.go
@@ -1,8 +1,21 @@
 package models
 
+import (
+	"github.com/sirchorg/go/common"
+)
+
 type Mail struct {
-	Recipient string `json:"recipient" firestore:"recipient"`
+	ID        string `json:"id" firestore:"id"`
 	Sender    string `json:"sender" firestore:"sender"`
+	Recipient string `json:"recipient" firestore:"recipient"`
 	Subject   string `json:"subject" firestore:"subject"`
 	Body      string `json:"body" firestore:"body"`
+}
+
+func (user *User) NewMail(app *common.App, recipient *User) *Mail {
+	return &Mail{
+		ID:        app.Token256(),
+		Sender:    user.Username,
+		Recipient: recipient.Username,
+	}
 }
