5711cd33b830312994a294715d2d66fec37a24ee
alex breadman <3dukltd@gmail.com>
2023-09-02 23:11:50 +0100

    update group

--- models/groups.go
+++ models/groups.go
@@ -3,21 +3,33 @@ package models
 import "github.com/google/uuid"
 
 type Group struct {
-	Meta        Internals
-	ID          string `json:"id" firestore:"id"`
-	Region      string `json:"region" firestore:"region"`
-	Admin       string `json:"admin" firestore:"admin"`
-	Name        string `json:"name" firestore:"name"`
-	Description string `json:"description" firestore:"description"`
-	Email       string `json:"email" firestore:"email"`
-	Website     string `json:"website" firestore:"website"`
-	// socials
-	Members []string `json:"members" firestore:"members"`
+	Meta Internals
+	ID   string `json:"id" firestore:"id"`
+	// controls whether anyone can instantly join a group
+	Open        bool     `json:"open" firestore:"open"`
+	Region      string   `json:"region" firestore:"region"`
+	Admin       string   `json:"admin" firestore:"admin"`
+	Moderators  []string `json:"moderators" firestore:"moderators"`
+	Name        string   `json:"name" firestore:"name"`
+	Description string   `json:"description" firestore:"description"`
+	Email       string   `json:"email" firestore:"email"`
+	Website     string   `json:"website" firestore:"website"`
 }
 
-func NewGroup(admin, region, name, descr string) *Group {
+func NewPrivateGroup(admin, region, name, descr string) *Group {
 	return &Group{
 		ID:          uuid.NewString(),
+		Region:      region,
+		Admin:       admin,
+		Name:        name,
+		Description: descr,
+	}
+}
+
+func NewPublicGroup(admin, region, name, descr string) *Group {
+	return &Group{
+		ID:          uuid.NewString(),
+		Open:        true,
 		Region:      region,
 		Admin:       admin,
 		Name:        name,
