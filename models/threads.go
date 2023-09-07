3e622d3bb27995bbd6c240a99eaaf02028dc0351
alex breadman <3dukltd@gmail.com>
2023-09-07 12:56:45 +0100

    tidied forum models

--- models/threads.go
+++ models/threads.go
@@ -8,44 +8,41 @@ import (
 
 func (user *User) NewThread(parent, title string) *Thread {
 	return &Thread{
-		Meta:      NewInternals(),
-		ID:        uuid.NewString(),
-		Username:  user.Username,
-		Title:     title,
-		Parent:    parent,
-		Timestamp: getTime(),
+		Meta:     NewInternals(),
+		ID:       uuid.NewString(),
+		Username: user.Username,
+		Title:    title,
+		Parent:   parent,
 	}
 }
 
 type Thread struct {
-	Meta      Internals
-	Type      string `json:"-" firestore:"-"`
-	Parent    string `json:"parent" firestore:"parent"`
-	ID        string `json:"id" firestore:"id"`
-	Username  string `json:"username" firestore:"username"`
-	Title     string `json:"title" firestore:"title"`
-	Timestamp int64  `json:"timestamp" firestore:"timestamp"`
+	Meta     Internals
+	Type     string `json:"-" firestore:"-"`
+	Parent   string `json:"parent" firestore:"parent"`
+	ID       string `json:"id" firestore:"id"`
+	Username string `json:"username" firestore:"username"`
+	Title    string `json:"title" firestore:"title"`
 }
 
 func (thread *Thread) Reply(user *User, content string) *ThreadReply {
 	return &ThreadReply{
-		ID:        uuid.NewString(),
-		Thread:    thread.ID,
-		Title:     thread.Title,
-		Username:  user.Username,
-		Content:   content,
-		Timestamp: getTime(),
+		Meta:     NewInternals(),
+		ID:       uuid.NewString(),
+		Thread:   thread.ID,
+		Title:    thread.Title,
+		Username: user.Username,
+		Content:  content,
 	}
 }
 
 type ThreadReply struct {
-	Meta      Internals
-	ID        string `json:"id" firestore:"id"`
-	Thread    string `json:"thread" firestore:"thread"`
-	Title     string `json:"title" firestore:"title"`
-	Username  string `json:"username" firestore:"username"`
-	Content   string `json:"content" firestore:"content"`
-	Timestamp int64  `json:"timestamp" firestore:"timestamp"`
+	Meta     Internals
+	ID       string `json:"id" firestore:"id"`
+	Thread   string `json:"thread" firestore:"thread"`
+	Title    string `json:"title" firestore:"title"`
+	Username string `json:"username" firestore:"username"`
+	Content  string `json:"content" firestore:"content"`
 }
 
 func (reply *ThreadReply) ThreadID() string {
