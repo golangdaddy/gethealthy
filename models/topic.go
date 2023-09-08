77e543cbb273404b1fbf220b1a8a99f3bfd55913
alex breadman <3dukltd@gmail.com>
2023-09-08 12:38:06 +0100

    add topic

--- models/topic.go
+++ models/topic.go
@@ -1,7 +1,19 @@
 package models
 
+import "github.com/google/uuid"
+
 type Topic struct {
 	Meta        Internals
+	ID          string
 	Name        string
 	Description string
+}
+
+func NewTopic(name, description string) *Topic {
+	return &Topic{
+		Meta:        NewInternals(),
+		ID:          uuid.NewString(),
+		Name:        name,
+		Description: description,
+	}
 }
