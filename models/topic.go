597492cc0558fd623eff53f85ef4ea285add26b1
alex breadman <3dukltd@gmail.com>
2023-09-08 15:14:42 +0100

    add topic

--- models/topic.go
+++ models/topic.go
@@ -4,9 +4,9 @@ import "github.com/google/uuid"
 
 type Topic struct {
 	Meta        Internals
-	ID          string
-	Name        string
-	Description string
+	ID          string `json:"id" firestore:"id"`
+	Name        string `json:"name" firestore:"name"`
+	Description string `json:"description" firestore:"description"`
 }
 
 func NewTopic(name, description string) *Topic {
