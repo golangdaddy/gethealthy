e3d5d72a63f9e9b51ffc8cac8310a06d7ce1f375
alex breadman <3dukltd@gmail.com>
2023-09-13 17:59:24 +0100

    mod product

--- models/products.go
+++ models/products.go
@@ -3,15 +3,20 @@ package models
 import "github.com/google/uuid"
 
 type Product struct {
-	Meta        Internals
-	ID          string  `json:"id" firestore:"id"`
-	Name        string  `json:"name" firestore:"name"`
-	Description string  `json:"description" firestore:"description"`
-	Price       float64 `json:"price" firestore:"price"`
-	Length      string  `json:"length" firestore:"length"`
-	Width       string  `json:"width" firestore:"width"`
-	Height      string  `json:"height" firestore:"height"`
-	Weight      string  `json:"weight" firestore:"weight"`
+	Meta         Internals
+	ID           string  `json:"id" firestore:"id"`
+	Name         string  `json:"name" firestore:"name"`
+	Description  string  `json:"description" firestore:"description"`
+	Price        float64 `json:"price" firestore:"price"`
+	Weight       string  `json:"weight" firestore:"weight"`
+	Length       string  `json:"length" firestore:"length"`
+	Width        string  `json:"width" firestore:"width"`
+	Height       string  `json:"height" firestore:"height"`
+	Manufacturer string  `json:"manufacturer" firestore:"manufacturer"`
+	Model        string  `json:"model" firestore:"model"`
+	SerialNumber string  `json:"sn" firestore:"sn"`
+	ISBN         string  `json:"isbn" firestore:"isbn"`
+	Country      string  `json:"country" firestore:"country"`
 }
 
 func (user *User) NewProduct() *Product {
