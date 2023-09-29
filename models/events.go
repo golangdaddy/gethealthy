e929aabcb560b0a5875cce286b19ff634b96f970
alex breadman <3dukltd@gmail.com>
2023-09-29 22:30:10 +0100

    fix groups api

--- models/events.go
+++ models/events.go
@@ -12,12 +12,14 @@ func (group *Group) NewEvent(name, description string, start int64) *Event {
 
 type Event struct {
 	Meta        Internals
+	Group       string  `json:"group" firestore:"group"`
 	ID          string  `json:"id" firestore:"id"`
 	Name        string  `json:"name" firestore:"name"`
 	Description string  `json:"description" firestore:"description"`
 	Location    string  `json:"location" firestore:"location"`
-	Lat         string  `json:"lat" firestore:"lat"`
-	Lng         string  `json:"lng" firestore:"lng"`
+	Lat         float64 `json:"lat" firestore:"lat"`
+	Lng         float64 `json:"lng" firestore:"lng"`
 	Time        int64   `json:"time" firestore:"time"`
-	Duration    float64 `json:"duration" firestore:"duration"`
+	// hours
+	Duration float64 `json:"duration" firestore:"duration"`
 }
