089262583d44ef94a8f6dbe9deb08480783334a9
alex breadman <3dukltd@gmail.com>
2023-10-25 13:40:13 +0100

    add comment

--- models/groups.go
+++ models/groups.go
@@ -38,31 +38,3 @@ func NewGroup(user *User, region, name, descr string, options GroupOptions) *Gro
 		Options:     options,
 	}
 }
-
-type Meeting struct {
-	Meta    Internals
-	Options GroupOptions
-	ID      string  `json:"id" firestore:"id"`
-	Title   string  `json:"title" firestore:"title"`
-	Address string  `json:"address" firestore:"address"`
-	Cost    float64 `json:"cost" firestore:"cost"`
-	Date    string  `json:"date" firestore:"date"`
-	Time    string  `json:"time" firestore:"time"`
-	// number of minutes
-	Duration int `json:"duration" firestore:"duration"`
-}
-
-func (group *Group) NewMeeting(title, address string, cost float64, date, time string, duration int) *Meeting {
-	meeting := &Meeting{
-		Meta:     NewInternals(),
-		ID:       uuid.NewString(),
-		Title:    title,
-		Address:  address,
-		Cost:     cost,
-		Date:     date,
-		Time:     time,
-		Duration: duration,
-	}
-	meeting.Meta.Parent = group.ID
-	return meeting
-}
---
+++ models/meeting.go
@@ -0,0 +1,48 @@
+package models
+
+import "github.com/google/uuid"
+
+type Meeting struct {
+	Meta    Internals
+	Options GroupOptions
+	ID      string  `json:"id" firestore:"id"`
+	Title   string  `json:"title" firestore:"title"`
+	Address string  `json:"address" firestore:"address"`
+	Cost    float64 `json:"cost" firestore:"cost"`
+	Date    string  `json:"date" firestore:"date"`
+	Time    string  `json:"time" firestore:"time"`
+	// number of minutes
+	Duration int `json:"duration" firestore:"duration"`
+}
+
+func (group *Group) NewMeeting(title, address string, cost float64, date, time string, duration int) *Meeting {
+	meeting := &Meeting{
+		Meta:     NewInternals(),
+		ID:       uuid.NewString(),
+		Title:    title,
+		Address:  address,
+		Cost:     cost,
+		Date:     date,
+		Time:     time,
+		Duration: duration,
+	}
+	meeting.Meta.Parent = group.ID
+	return meeting
+}
+
+type MeetingComment struct {
+	Meta    Internals
+	User    UserRef
+	ID      string `json:"id" firestore:"id"`
+	Content string `json:"content" firestore:"content"`
+}
+
+func (group *Group) MeetingComment(content string) *MeetingComment {
+	comment := &MeetingComment{
+		Meta:    NewInternals(),
+		ID:      uuid.NewString(),
+		Content: content,
+	}
+	comment.Meta.Parent = group.ID
+	return comment
+}
