e4a3b418af14f84a4d681f8ba6d1a4d447e01998
alex breadman <3dukltd@gmail.com>
2023-10-31 09:26:51 +0000

    add comment

--- models/meeting.go
+++ models/meeting.go
@@ -24,7 +24,7 @@ type Meeting struct {
 
 func (group *Group) NewMeeting(title, address string, cost float64, date, theTime string, duration int) (*Meeting, error) {
 
-	t, err := time.Parse("Fri Oct 20 2023 00:00:00 GMT+0100", date)
+	t, err := time.Parse("2023-10-06T23:00:00.000Z", date)
 	if err != nil {
 		return nil, err
 	}
